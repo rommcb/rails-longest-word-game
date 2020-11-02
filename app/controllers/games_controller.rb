0e8d1480c777d0282244bb4c066c4276bada6a46
Romain Borremans <romain.borremans@gmail.com>
2020-11-02 22:18:24 +0100

    importing solution

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,3 +1,5 @@
+require "open-uri"
+
 class GamesController < ApplicationController
 
     def new
@@ -5,8 +7,22 @@ class GamesController < ApplicationController
     end
 
     def score
-       @word = params[:word]
-       @letters = params[:letters]
+       @word = params[:word].upcase.split("")
+       @letters = params[:letters].split.join
+       @included = included?(@word, @letters)
+       @english_word = english_word?(@word)
+    end
+
+    private
+
+    def included?(word, letters)
+      word.all? { |letter| word.count(letter) <= letters.count(letter) }
+    end
+  
+    def english_word?(word)
+      response = open("https://wagon-dictionary.herokuapp.com/#{params[:word]}")
+      json = JSON.parse(response.read)
+      json['found']
     end
 
 end
